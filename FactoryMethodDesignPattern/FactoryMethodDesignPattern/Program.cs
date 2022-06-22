f3970fbad819e4d8d29214df05f6ef2d76cc7ed2
Maharoz <mahruj66@gmail.com>
2022-06-22 23:55:26 +0600

    update

--- FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
+++ FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
@@ -95,6 +95,7 @@ namespace FactoryMethodDesignPattern
             CreditCard creditCard = new PlatinumFactory().CreateProduct();
             if (creditCard != null)
             {
+
                 Console.WriteLine("Card Type : " + creditCard.GetCardType());
                 Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
                 Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
